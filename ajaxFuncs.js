c5440be33b1f873742f2d70559c3febd53714228
Jordan Apgar <twistedphrame@gmail.com>
2015-04-26 07:21:40 -0400

    added confirm  popups

--- ajaxFuncs.js
+++ ajaxFuncs.js
@@ -26,25 +26,28 @@ function addToCart(prodID, dropDownID) {
 
 function removeItemFromCart(prodID) {
   "use strict";
-  var xhr;
-  if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) {
-      xhr = new ActiveXObject("Msxml2.XMLHTTP");
-  } else {
-      throw new Error("Ajax is not supported by this browser");
+  var r = confirm("Are you sure you want to removed the selected item from the cart?");
+	if (r == true) {
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } else {
+        throw new Error("Ajax is not supported by this browser");
+    }
+    //what do I do when i get a response back
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 && xhr.status < 300) {
+                window.location.replace('shopping_cart.php', '_SELF');
+            }
+        }
+    }
+    xhr.open('POST', 'remove_from_cart.php');
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("productid="+prodID);
   }
-  //what do I do when i get a response back
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-          if (xhr.status === 200 && xhr.status < 300) {
-              window.location.replace('shopping_cart.php', '_SELF');
-          }
-      }
-  }
-  xhr.open('POST', 'remove_from_cart.php');
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send("productid="+prodID);
 }
 
 function order(username) {
@@ -72,29 +75,40 @@ function order(username) {
 
 function updateOrderStatus(prodID, transID, username, newStatus, pageToReturnTo) {
 "use strict";
-  var xhr;
-  if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) {
-      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+  var status;
+  if (newStatus == 'c') {
+    status = 'canceled';
+  } else if (new status == 'p') {
+    status = 'pending';
   } else {
-      throw new Error("Ajax is not supported by this browser");
+    status = 'shipped';
   }
-  //what do I do when i get a response back
-  xhr.onreadystatechange = function () {
-      if (xhr.status == 404) {
-        alert("Oh no, it does not exist!");
-      }
-      if (xhr.readyState === 4) {
-          if (xhr.status === 200 && xhr.status < 300) {
-            window.location.replace(pageToReturnTo, '_SELF');
-          }
-      }
+  var r = confirm("The order status will be updates to '" + status + "', do you want to continue?");
+  if (r) {
+    var xhr;
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } else {
+        throw new Error("Ajax is not supported by this browser");
+    }
+    //what do I do when i get a response back
+    xhr.onreadystatechange = function () {
+        if (xhr.status == 404) {
+          alert("Oh no, it does not exist!");
+        }
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 && xhr.status < 300) {
+              window.location.replace(pageToReturnTo, '_SELF');
+            }
+        }
+    }
+    xhr.open('POST', 'order_status_update.php');
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("productid="+prodID
+           +"&transactionid="+transID
+           +"&username="+username
+           +"&status="+newStatus);
   }
-  xhr.open('POST', 'order_status_update.php');
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.send("productid="+prodID
-         +"&transactionid="+transID
-         +"&username="+username
-         +"&status="+newStatus);
 }
