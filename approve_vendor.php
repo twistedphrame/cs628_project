c62b43b6a74cb93f209d228cfa4bae35e98c8312
twistedphrame <twistedphrame@gmail.com>
2015-04-27 17:24:57 -0400

    we now get an approve email for the vendor

--- approve_vendor.php
+++ approve_vendor.php
@@ -11,10 +11,19 @@
     include("dbc.php");
     
     
+    $userInfo = selectSingleCustomer($dbc, $_POST[USER_TABLE::$USER_NAME]);
+    
     $q = 'UPDATE ' . USER_TABLE::$NAME . ' SET '
           .USER_TABLE::$APPROVED.'=\'1\' WHERE '.USER_TABLE::$USER_NAME.' = \''.$_POST[USER_TABLE::$USER_NAME].'\'';
     $r = mysqli_query($dbc, $q);
     if($r) {
+      $to = $userInfo[USER_TABLE::$EMAIL];
+			$subject = 'You Have Been Approved';
+      $message = "Hi ". $userInfo[USER_TABLE::$FIRST_NAME] . ' You can now add products to sell to our system!';
+  		$headers = 'From: Online Shopping System' . "\r\n" .
+    								'Reply-To: ' . "\r\n" .
+    								'X-Mailer: PHP/' . phpversion();
+      mail($to, $subject, $message, $headers);
       return true;
     }
     return false;
