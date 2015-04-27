f4e12c1b7f8ac292044ecb5b8f4c1aaf42a04b3c
twistedphrame <twistedphrame@gmail.com>
2015-04-27 17:56:38 -0400

    emails should be working

--- approve_product.php
+++ approve_product.php
@@ -13,6 +13,14 @@
     $product->setProductID($_POST[PRODUCT_TABLE::$PROD_ID])
             ->setApproved(true)
             ->update($dbc);
+    require_once("include/sql_queries.php");
+    $product = selectSingleProduct($dbc, $_POST[PRODUCT_TABLE::$PROD_ID]);
+    $userInfo = selectSingleCustomer($dbc, $product[PRODUCT_TABLE::$VEND_ID]);
+    $subject = 'You Product is ready for sale!';
+    $message = "Hi ". $userInfo[USER_TABLE::$FIRST_NAME . ' Your product'.$product[PRODUCT_TABLE::$PROD_NAME].' can now be sold!';
+    $headers = 'From: Online Shopping System' . "\r\n" .
+    'Reply-To: ' . "\r\n" .	'X-Mailer: PHP/' . phpversion();
+    mail($userInfo[USER_TABLE::$EMAIL], $subject, $message, $headers);
   } else {
     header('LOCATION: products.php');
   }
