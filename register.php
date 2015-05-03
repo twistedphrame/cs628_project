c555d1d0f1ff03fef93bba74e2fccf0ad0b8d4ac
Jordan Apgar <twistedphrame@gmail.com>
2015-05-03 14:40:48 -0400

    remove the double header

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Class Registration</title>
+<title>Shopping Registration</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 </head>
 <body>
@@ -12,10 +12,6 @@
   ?>
 
 <div id="container">
-	<div id="header">
-	<h1 style ="padding:20px 100px">Online Shopping System Registration</h1>
-	</div>
-	
 	<div id="main" align="center">
 		<div style="color:red">
 		<?php 
